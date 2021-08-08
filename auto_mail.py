3950180d68c63cda47955294c63c255bffeac840
Manav Notaria <60823881+manavnotaria@users.noreply.github.com>
2021-08-08 16:10:56 +0530

    Add files via upload

---
+++ auto_mail.py
@@ -0,0 +1,72 @@
+# Python code to illustrate Sending mail with attachments
+# from your Gmail account
+
+# libraries to be imported
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+fromaddr = "Sender's Mail"
+arr=["Receiver's Mail"]
+for i in range(len(arr)-1):
+
+    toaddr = arr[i]
+
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = "GREETINGS!"
+
+    # string to store the body of the mail
+    body = """
+
+    Hi, How are you?
+
+    """
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    filename = "XYZ.pdf"
+    attachment = open("FILE LOCATION HERE", "rb")
+
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+
+    # encode into base64
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(fromaddr, "SENDER's Email Password")
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+
+    # terminating the session
+    s.quit()
